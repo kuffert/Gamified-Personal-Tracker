c44335cc1b6c653fa10fe1aede97a99f200d8aab
luggaboom <yvettekim92@gmail.com>
2015-11-08 17:54:33 -0500

    Filling Activity fields; adding Skill class

--- Assets/Non-Attached Scripts/Activity.cs
+++ Assets/Non-Attached Scripts/Activity.cs
@@ -4,16 +4,26 @@ using System.Collections.Generic;
 public class Activity {
 
 	string title;
-	List<Activity> topicsOfInterest;
+	string format;
+	List<string> topicsOfInterest;
 	string description;
 	string startDate; // needs a proper date format
 	string endDate;   // needs a proper date format
 	bool joinAnytime;
 	int lengthOfEngagement; // May need to make a seperate enum for this. day, week, month, semester, year, summer
 	string location;
-	string recurrence;
-	int academicStanding; // May need to make this an enum as well. year 1,2,3,4,5
+	List<string> recurrence;
+	bool coop;
+	List<int> academicStanding; // May need to make this an enum as well. year 1,2,3,4,5,NU IN, last year or last semester
 	string major;
+	bool resident;
+	string sponsor;
+	string contactName;
+	string contactOffice;
+	string contactEmail;
+	List<string> learningOutcomes;
+	List<Skill> skills;
+	string engagement;
 
     Activity()
     {
---
+++ Assets/Non-Attached Scripts/Skill.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Skill {
+	
+	string id;
+	string skill;
+	List<string> opportunities;
+
+	
+	Skill()
+	{
+	}
+}
