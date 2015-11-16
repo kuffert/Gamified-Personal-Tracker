6f41245a761156bb9aa6e386852496e057139e15
Chris Kuffert <chriskuffert@comcast.net>
2015-11-16 18:26:04 -0500

    Added a new folder with testing materials.

---
+++ Assets/Testing/Testing.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Testing : MonoBehaviour {
+
+	// All Tests should be written or called inside this function, that way they are only called once.
+	void Start () {
+
+        // Test Example
+        bool exampleTestResult = true;
+        Debug.Log("Example Test result: " + exampleTestResult);
+	}
+}
