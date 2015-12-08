79506262ec27de5449422ceb5f9891c655102e21
petracles <petracles@gmail.com>
2015-12-07 19:24:33 -0500

    Updated Login Scene
    1) User now selects a button for grad year
    2) User Major is substring-ed if too long

--- Assets/SceneScripts/Login Scripts/Login.cs
+++ Assets/SceneScripts/Login Scripts/Login.cs
@@ -13,7 +13,6 @@ public class Login : MonoBehaviour {
 	private string userNameString = "Username";
 	private string userPasswordString = "Password";
 	private string userMajorString = "Major";
-	private string userYearString = "Year";
 
 	// Window for GUI:
 	private Rect windowRect = new Rect(0, 0, Screen.width, Screen.height);
@@ -40,19 +39,30 @@ public class Login : MonoBehaviour {
 		GUI.Label (new Rect (Screen.width / 4, 0.15f * Screen.height / 5f, Screen.width / 2, Screen.height / 10), "Enter a Username:", labelStyle);
 		GUI.Label (new Rect (Screen.width / 4, 1.15f * Screen.height / 5f, Screen.width / 2, Screen.height / 10), "Enter the Password:", labelStyle);
 		GUI.Label (new Rect (Screen.width / 4, 2.15f * Screen.height / 5f, Screen.width / 2, Screen.height / 10), "Enter a Major:", labelStyle);
-		GUI.Label (new Rect (Screen.width / 4, 3.15f * Screen.height / 5f, Screen.width / 2, Screen.height / 10), "Enter a Year:", labelStyle);
+		GUI.Label (new Rect (Screen.width / 4, 3.15f * Screen.height / 5f, Screen.width / 2, Screen.height / 10), "Choose a Year:", labelStyle);
 
 		// TextFields:
-		userNameString		= GUI.TextField(new Rect(Screen.width/3, 0.5f * Screen.height/5f, Screen.width/3, Screen.height/10), userNameString, 10);
+		userNameString		= GUI.TextField(new Rect(Screen.width/3, 0.5f * Screen.height/5f, Screen.width/3, Screen.height/10), userNameString, 20);
 		userPasswordString	= GUI.PasswordField(new Rect(Screen.width/3, 1.5f * Screen.height/5, Screen.width/3, Screen.height/10), userPasswordString, "*"[0], 10);
-		userMajorString		= GUI.TextField(new Rect(Screen.width/3, 2.5f * Screen.height/5f, Screen.width/3, Screen.height/10), userMajorString, 10);
-		userYearString		= GUI.TextField(new Rect(Screen.width/3, 3.5f * Screen.height/5f, Screen.width/3, Screen.height/10), userYearString, 10);
+		userMajorString		= GUI.TextField(new Rect(Screen.width/3, 2.5f * Screen.height/5f, Screen.width/3, Screen.height/10), userMajorString, 50);
 
+		// Buttons for Year:
+		for (int i = 0; i < 5; i++) {
+			string yearString = (i + 2017).ToString();
+			if (GUI.Button(new Rect((float)i * Screen.width/4f, 3.5f * Screen.height/5f, Screen.width/4, Screen.height/12), yearString)) {
+				Debug.Log (yearString);
+				AppController.appController.setYear (int.Parse(yearString));
+			}
+		}
+
+		// Button go "GO!"
 		if (GUI.Button (new Rect(Screen.width/3, 4.25f * Screen.height/5, Screen.width/3, Screen.height/10), "GO!")) {
 			if (userPasswordString == superSecretPassword) {
 				AppController.appController.setUsername (userNameString);
+				if (userMajorString.Length > 8) {
+					userMajorString = userMajorString.Substring(0, 8) + "...";
+				}
 				AppController.appController.setMajor (userMajorString);
-				AppController.appController.setYear (int.Parse(userYearString));
 				AppController.appController.Save ();
 				Debug.Log ("Setting Data and Logging In...");
 				Application.LoadLevel ("Profile");
