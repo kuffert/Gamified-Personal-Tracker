e72d3497f39d44cf1f4574807582deeee25187e7
petracles <petracles@gmail.com>
2015-12-07 23:22:28 -0500

    Updated Login formatting
    Input Fields and Labels look better now.

--- Assets/SceneScripts/Login Scripts/Login.cs
+++ Assets/SceneScripts/Login Scripts/Login.cs
@@ -8,6 +8,8 @@ public class Login : MonoBehaviour {
 
 	// Label GUIStyle:
 	private GUIStyle labelStyle = new GUIStyle ();
+	private GUIStyle fieldStyle = new GUIStyle ();
+	private GUIStyle yearButtonStyle = new GUIStyle ();
 
 	// TextField Strings:
 	private string userNameString = string.Empty;
@@ -31,6 +33,12 @@ public class Login : MonoBehaviour {
         }
 		labelStyle.alignment = TextAnchor.MiddleCenter;
 		labelStyle.fontSize = Screen.width / 10;
+		labelStyle.normal.textColor = Color.white;
+		fieldStyle.alignment = TextAnchor.MiddleCenter;
+		fieldStyle.fontSize = Screen.width / 12;
+		fieldStyle.normal.textColor = Color.red;
+		yearButtonStyle.fontSize = Screen.width / 14;
+		yearButtonStyle.normal.textColor = Color.red;
 	}
 	
 	// Update is called once per frame
@@ -48,22 +56,23 @@ public class Login : MonoBehaviour {
 		GUI.Label (new Rect (Screen.width / 4, 1.15f * Screen.height / 5f, Screen.width / 2, Screen.height / 10), enterPasswordString, labelStyle);
 
         // TextFields:
-        userNameString = GUI.TextField(new Rect(Screen.width / 3, 0.5f * Screen.height / 5f, Screen.width / 3, Screen.height / 10), userNameString, 20, labelStyle);
-        userPasswordString = GUI.PasswordField(new Rect(Screen.width / 3, 1.5f * Screen.height / 5, Screen.width / 3, Screen.height / 10), userPasswordString, "*"[0], 10, labelStyle);
+		userNameString = GUI.TextField(new Rect(Screen.width / 3, 0.5f * Screen.height / 5f, Screen.width / 3, Screen.height / 10), userNameString, 20, fieldStyle);
+		userPasswordString = GUI.PasswordField(new Rect(Screen.width / 3, 1.5f * Screen.height / 5, Screen.width / 3, Screen.height / 10), userPasswordString, "*"[0], 10, fieldStyle);
 
         if (!AppController.appController.getCreatedAccount())
         {
             GUI.Label(new Rect(Screen.width / 4, 2.15f * Screen.height / 5f, Screen.width / 2, Screen.height / 10), "Enter a Major:", labelStyle);
             GUI.Label(new Rect(Screen.width / 4, 3.15f * Screen.height / 5f, Screen.width / 2, Screen.height / 10), "Choose a Year:", labelStyle);
-            userMajorString = GUI.TextField(new Rect(Screen.width / 3, 2.5f * Screen.height / 5f, Screen.width / 3, Screen.height / 10), userMajorString, 50, labelStyle);
+            userMajorString = GUI.TextField(new Rect(Screen.width / 3, 2.5f * Screen.height / 5f, Screen.width / 3, Screen.height / 10), userMajorString, 50, fieldStyle);
 
             // Buttons for Year:
-            for (int i = 0; i < 5; i++)
+            for (int i = 1; i < 6; i++)
             {
                 string yearString = (i + 2017).ToString();
-                if (GUI.Button(new Rect((float)i * Screen.width / 4f, 3.5f * Screen.height / 5f, Screen.width / 4, Screen.height / 12), yearString))
+                if (GUI.Button(new Rect(((float)i * Screen.width / 5f) - 55, 3.5f * Screen.height / 5f, Screen.width / 4, Screen.height / 12), yearString, yearButtonStyle))
                 {
                     Debug.Log(yearString);
+					yearButtonStyle.normal.textColor = Color.white;
                     AppController.appController.setYear(int.Parse(yearString));
                 }
             }
